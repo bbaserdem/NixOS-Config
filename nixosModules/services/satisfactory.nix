23deeff38ea53d768acdd9ce916325fb7290e860
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-03 14:52:09 -0500

    Things

--- nixosModules/services/satisfactory.nix
+++ nixosModules/services/satisfactory.nix
@@ -5,31 +5,6 @@
   ...
 }:
 with lib; {
-  users.users.satisfactory = {
-    home = "/var/lib/satisfactory";
-    createHome = true;
-    isSystemUser = true;
-    group = "satisfactory";
-  };
-  users.groups.satisfactory = {};
-
-  # boot.kernel.sysctl."net.ipv6.conf.eth0.disable_ipv6" = true;
-  networking.enableIPv6 = false;
-
-  # nixpkgs.config.allowUnfree = true;
-  #
-  networking = {
-    firewall = {
-      allowedUDPPorts = [15777 15000 7777 27015];
-      allowedUDPPortRanges = [
-        {
-          from = 27031;
-          to = 27036;
-        }
-      ];
-      allowedTCPPorts = [27015 27036];
-    };
-  };
 
   # -beta experimental \
   systemd.services.satisfactory = {
