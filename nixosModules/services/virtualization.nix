2fc574513716ca70659b4d09257149b2a7a9d917
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-12 13:32:52 -0500

    Added docker support

--- nixosModules/services/virtualization.nix
+++ nixosModules/services/virtualization.nix
@@ -8,9 +8,32 @@ with lib; {
   # Enable libvirtd
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-  
+
+  # Enable docker
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
+  # Add our user to docker group 
+  users.extraGroups.docker.members = [ myNixOS.userName ];
+
+  # Example docker as systemd service
+  #virtualisation.oci-containers = {
+  #  backend = "docker";
+  #  containers = {
+  #    foo = {
+  #      # ...
+  #    };
+  #  };
+  #};
+
   # Install virtio-win drivers
   environment.systemPackages = with pkgs; [
     virtio-win
+    docker-client
   ];
 }
