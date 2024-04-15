4af1ce13ad444ac27092815ad5e1c0749caac4a6
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-15 15:34:13 -0500

    Things

--- nixosModules/features/grub.nix
+++ nixosModules/features/grub.nix
@@ -1,11 +1,14 @@
 # Module that configures grub
 
 { pkgs, lib, ... }: {
-  myNixOS.sharedSettings.grubTheme = lib.mkDefault "orange";
+  #myNixOS.sharedSettings.grubTheme = lib.mkDefault "orange";
   boot.loader.grub = {
     enable = true;
     useOSProber = true;
     memtest86.enable = true;
     theme = pkgs.sleek-grub-theme;
   };
+  environment.systemPackages = [
+    pkgs.sleek-grub-theme
+  ];
 }
