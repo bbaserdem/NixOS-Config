cd69adfe5353947da19c737143063e04a438edb6
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-15 15:43:10 -0500

    Added config to reference myNixOS

--- nixosModules/services/nm.nix
+++ nixosModules/services/nm.nix
@@ -10,5 +10,5 @@ with lib; {
   # Enable network manager
   networking.networkmanager.enable = true;
   # Add default user to networkmanager
-  users.extraGroups.networkmanager.members = [ myNixOS.userName ];
+  users.extraGroups.networkmanager.members = [ config.myNixOS.userName ];
 }
