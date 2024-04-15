6f8f03471c406bff2cec7772d1a7f2f9a68a8e9b
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-15 15:15:03 -0500

    Things

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,5 +1,4 @@
 # Common configuration options for all the hosts
-
 {
   inputs,
   outputs,
@@ -17,7 +16,7 @@
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
+      #outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
 
