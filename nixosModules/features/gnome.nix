8a208b263d15e7b372fae998bdedc7a28cdf7d97
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-15 15:16:52 -0500

    Things

--- nixosModules/features/gnome.nix
+++ nixosModules/features/gnome.nix
@@ -17,10 +17,10 @@
     desktopManager = {
       gnome.enable = true;
     };
-    # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-    systemd.services = {
-      "getty@tty1".enable = false;
-      "autovt@tty1".enable = false;
-    };
+  };
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services = {
+    "getty@tty1".enable = false;
+    "autovt@tty1".enable = false;
   };
 }
