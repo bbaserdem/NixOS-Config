a50e3d058279eabe36686d1fd1178cca84c16424
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-15 14:53:09 -0500

    Removed invalid block

--- nixosModules/services/media.nix
+++ nixosModules/services/media.nix
@@ -31,12 +31,12 @@
 	  '';
   };
   # For unstable; the above block is
-  services.pipewire.wireplumber.extraLuaConfig.bluetooth."51-bluez-config" = ''
-	    bluez_monitor.properties = {
-		    ["bluez5.enable-sbc-xq"] = true,
-		    ["bluez5.enable-msbc"] = true,
-		    ["bluez5.enable-hw-volume"] = true,
-		    ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-	    }
-    '';
+  #services.pipewire.wireplumber.extraLuaConfig.bluetooth."51-bluez-config" = ''
+	#    bluez_monitor.properties = {
+	#	    ["bluez5.enable-sbc-xq"] = true,
+	#	    ["bluez5.enable-msbc"] = true,
+	#	    ["bluez5.enable-hw-volume"] = true,
+	#	    ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+	#    }
+  #  '';
 }
