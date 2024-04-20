ecf1014de8d9c8656c592f2292f8442d2c89966b
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-19 22:51:05 -0500

    Why was I on 23.05? Fixed this

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
