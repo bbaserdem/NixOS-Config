c366e79e96e25efc5fb2b01fb55f40cab0b364a2
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-22 13:43:12 -0500

    Configured nnn

--- home-manager/batuhan/configs/nnn/default.nix
+++ home-manager/batuhan/configs/nnn/default.nix
@@ -20,6 +20,38 @@ in {
     };
     plugins = {
       src = "${pkgs.nnn}/share/plugins";
+      mappings = {
+        n = "autojump";
+        d = "bulknew";
+        c = "chksum";
+        D = "diffs";
+        f = "finder";
+        F = "fzcd";
+        o = "fzopen";
+        "+" = "fzplug";
+        r = "gitroot";
+        e = "gpge";
+        v = "imgview";
+        " " = "launch";
+        i = "ipinfo";
+        t = "mimelist";
+        M = "mtpmount";
+        m = "nmount";
+        p = "preview-tui";
+        k = "pskill";
+        r = "renamer";
+        y = "rsynccp";
+        s = "splitjoin";
+        S = "suedit";
+        x = "togglex";
+        u = "umounttree";
+      };
     };
+  };
+  # Config options from environment
+  home.sessionVariables = {
+    NNN_OPTS = "adD";
+    NNN_COLORS = "6423";
+    NNN_TMPFILE = '/tmp/.lastd';
   };
 }
