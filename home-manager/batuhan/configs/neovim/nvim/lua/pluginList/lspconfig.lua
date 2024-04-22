cb514acf4a39d998ef7887faf288b8c675794283
bbaserdem <baserdem.batuhan@gmail.com>
2024-04-22 18:39:36 -0500

    Adding lsp to nvim

---
+++ home-manager/batuhan/configs/neovim/nvim/lua/pluginList/lspconfig.lua
@@ -0,0 +1,12 @@
+-------------------------------------------------
+-- name : nvim-lspconfig
+-- url  : https://github.com/neovim/nvim-lspconfig
+---------------------------------------------------
+-- Comment out blocks of text
+--local wk = require("which-key")
+
+local M = {
+    "neovim/nvim-lspconfig",
+}
+
+return M
