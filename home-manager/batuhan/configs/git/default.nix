377f5bc5e09b8ecb2cc517af9ed4878fc8df205c
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2024-04-25 14:08:34 -0400

    Added lazygit config to git folder

--- home-manager/batuhan/configs/git/default.nix
+++ home-manager/batuhan/configs/git/default.nix
@@ -24,4 +24,15 @@
       };
     };
   };
+  programs.lazygit = {
+    enable = true;
+    package = pkgs.lazygit;
+    settings = {
+      git = {
+        commit.autoWrapWidth = 80;
+        mainBranches = [ "main" "master" ];
+        parseEmoji = true;
+      };
+    };
+  };
 }
