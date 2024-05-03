126e81c865df7a40daab2f2447dd5cdc654338fe
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2024-05-03 14:56:03 -0400

    Trying to fix erroneus file call

--- home-manager/batuhan/configs/neovim/nvim/after/ftplugin/lua.lua
+++ home-manager/batuhan/configs/neovim/nvim/after/ftplugin/lua.lua
@@ -2,11 +2,11 @@
 -- Lua lsp config
 
 -- Load cmp capabilities
-local lsp_common = require("lsp-common")
+local lspc = require("lsp-common")
 
 require("lspconfig").lua_ls.setup {
     -- on_attach = <function>,
-    capabilities = lsp_common.capabilities_with_cmp,
+    capabilities = lspc.capabilities_with_cmp,
     Lua = {
         workspace = { checkThirdParty = false, },
         telemetry = { enable = false, },
