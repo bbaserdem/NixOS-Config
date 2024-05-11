8a7ee9029748879a00672e38eb3d3edba65ffc92
bbaserdem <baserdemb@gmail.com>
2024-05-10 23:12:25 -0400

    Home manager + sops
    Made sops work with home-manager, and look for the keyfile in ssh directory

--- home-manager/batuhan/configs/sops/default.nix
+++ home-manager/batuhan/configs/sops/default.nix
@@ -1,14 +1,15 @@
 # Configuring SOPS so that the key is in the location we want
 {
   pkgs,
+  config,
   ...
 }: {
   # Set environment variable so systemd can find it
   systemd.user.sessionVariables = {
     EDITOR = "nvim";
-    SOPS_AGE_KEY_FILE = "~/.ssh/batuhan_age_keys.txt";
+    SOPS_AGE_KEY_FILE = "${config.home.homeDirectory}/.ssh/batuhan_age_keys.txt";
   };
   home.sessionVariables = {
-    SOPS_AGE_KEY_FILE = "~/.ssh/batuhan_age_keys.txt";
+    SOPS_AGE_KEY_FILE = "${config.home.homeDirectory}/.ssh/batuhan_age_keys.txt";
   };
 }
