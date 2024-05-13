3ab2bef9d5afe5c202537cf4e8df62978bdbb8f2
bbaserdem <baserdemb@gmail.com>
2024-05-13 15:57:37 -0400

    Home - Work
    Added work symlink to yel-ana

--- home-manager/batuhan/yel-ana.nix
+++ home-manager/batuhan/yel-ana.nix
@@ -14,8 +14,9 @@
   imports = [
     ./default.nix
   ];
-  # Create the media symlink
+  # Create data symlinks
   home.file."Media".source = config.lib.file.mkOutOfStoreSymlink "/home/data/Media";
+  home.file."Work".source =  config.lib.file.mkOutOfStoreSymlink "/home/data/Work";
   # Disable autorandr in gnome for now
   services.autorandr.enable = lib.mkOverride 500 false;
 }
