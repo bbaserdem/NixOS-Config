8a738d1e3112e04956a90d9e9b855407bc7f37c4
bbaserdem <baserdemb@gmail.com>
2024-05-19 17:17:56 -0400

    Nixos ~nix-index
    Added override to remove option

--- nixos/bundles/nix.nix
+++ nixos/bundles/nix.nix
@@ -1,5 +1,7 @@
 # Bundling nix related software
 { pkgs, lib, config, ... }: {
+  # This needs disabling for nix-index flake to work
+  programs.command-not-found.enable = false;
   environment.systemPackages = with pkgs; [
     unstable.nh
     nix-output-monitor
