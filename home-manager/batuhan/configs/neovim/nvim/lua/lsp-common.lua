335768fb4a5a6ae35e6e8d2c965cb8859b2bee44
bbaserdem <baserdemb@gmail.com>
2024-05-19 21:49:15 -0400

    Nvim - lsp keybinds
    Changed keybinds to which-key

--- home-manager/batuhan/configs/neovim/nvim/lua/lsp-common.lua
+++ home-manager/batuhan/configs/neovim/nvim/lua/lsp-common.lua
@@ -8,48 +8,38 @@ vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(_args)
         local bufnr = _args.buf
         -- local client = vim.lsp.get_client_by_id(_args.data.client_id)
-
-        -- Keybinding function
-        local bufmap = function(_keys, _func, _desc)
-            vim.keymap.set("n", _keys, _func, {
-                buffer = bufnr,
-                desc = _desc,
-            })
-        end
-        -- Generic LSP keybinds
-        bufmap("<leader>lr", vim.lsp.buf.rename, "LSP rename")
-        bufmap("<leader>la", vim.lsp.buf.code_action, "LSP code action")
-
-        bufmap("<leader>ld", vim.lsp.buf.definition,
-            "LSP definition")
-        bufmap("<leader>lD", vim.lsp.buf.type_definition,
-            "LSP type definition")
-        bufmap("<leader>li", vim.lsp.buf.declaration,
-            "LSP declaration")
-        bufmap("<leader>lI", vim.lsp.buf.implementation,
-            "LSP implementation")
-
-        bufmap("<leader>lt", require("telescope.builtin").lsp_references,
-            "LSP references")
-        bufmap("<leader>ls", require("telescope.builtin").lsp_document_symbols,
-            "LSP symbols")
-        bufmap("<leader>lS", require("telescope.builtin").lsp_dynamic_workspace_symbols,
-            "LSP symbols (workspace)")
-
-        bufmap("K", vim.lsp.buf.hover, "LSP hover menu")
-
-        vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-            vim.lsp.buf.format()
-        end, {})
+        require("which-key").register({
+            l = {
+                name = "LSP functions",
+                r = { vim.lsp.buf.rename,
+                    "Rename" },
+                a = { vim.lsp.buf.code_action,
+                    "code Action" },
+                d = { vim.lsp.buf.definition,
+                    "Definition" },
+                t = { vim.lsp.buf.type_definition,
+                    "Type definition" },
+                i = { vim.lsp.buf.declaration,
+                    "(Info) declaration" },
+                u = { vim.lsp.buf.implementation,
+                    "(Use) implementation" },
+                p = { require("telescope.builtin").lsp_references,
+                    "(Pointer) references - list" },
+                s = { require("telescope.builtin").lsp_document_symbols,
+                    "Symbols - list" },
+                w = { require("telescope.builtin").lsp_dynamic_workspace_symbols,
+                    "Workspace symbols - list" },
+                k = { vim.lsp.buf.hover,
+                    "hover menu" },
+                f = { vim.lsp.buf.format,
+                    "Format", },
+            },
+        }, {
+            prefix = "<leader>",
+            buffer = bufnr,
+        })
     end,
 })
-require("which-key").register(
-    {
-        l = {
-            name = "LSP functions",
-        },
-    }, { prefix = "<leader>", }
-)
 
 -- Load cmp capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
