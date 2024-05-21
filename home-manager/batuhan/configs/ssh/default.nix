5d1dce5ac6b986f48f89320bb0f8b51ee5e49080
bbaserdem <baserdemb@gmail.com>
2024-05-21 12:57:24 -0400

    Home + ssh setup
    Added more ssh setup to home

--- home-manager/batuhan/configs/ssh/default.nix
+++ home-manager/batuhan/configs/ssh/default.nix
@@ -25,6 +25,25 @@
           PubkeyAcceptedKeyTypes = "+ssh-rsa";
         };
       };
+      hopper = {
+        # Not working for some reason
+        user = "batu";
+        hostname = "hopper.cshl.edu";
+        identitiesOnly = true;
+        extraOptions = {
+          HostKeyAlgorithms = "ssh-rsa";
+          IdentityFile = "~/.ssh/id_rsa_HOPPER";
+          PubkeyAcceptedAlgorithms = "+ssh-rsa";
+          PubkeyAcceptedKeyTypes = "+ssh-rsa";
+        };
+      };
+      haibara = {
+        # Not working for some reason
+        user = "batuhan";
+        hostname = "haibara.cshl.edu";
+        identitiesOnly = true;
+        extraOptions.IdentityFile = "~/.ssh/id_ed25519_HAIBARA";
+      };
     };
   };
 }
