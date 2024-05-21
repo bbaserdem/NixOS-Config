ae44de906ec33923292697105ab2bb5b807dfe28
bbaserdem <baserdemb@gmail.com>
2024-05-21 17:32:04 -0400

    Home -gnome
    Rolled back some gnome customizations due to home-manager versioning

--- home-manager/batuhan/configs/gnome/default.nix
+++ home-manager/batuhan/configs/gnome/default.nix
@@ -5,14 +5,15 @@
   ...
 }: {
   # Some gnome extensions
-  programs.gnome-shell = {
-    enable = true;
-    extensions = with pkgs; [
-      { package = gnomeExtensions.appindicator; }
-      { package = gnomeExtensions.wireless-hid; }
-      { package = gnomeExtensions.usd-try; }
-    ];
-  };
+  # This needs home-manager unstable, enable after 24.05
+  # programs.gnome-shell = {
+    # enable = true;
+    # extensions = with pkgs; [
+      # { package = gnomeExtensions.appindicator; }
+      # { package = gnomeExtensions.wireless-hid; }
+      # { package = gnomeExtensions.usd-try; }
+    # ];
+  # };
   # Enable firefox to integrate too
   programs.firefox.enableGnomeExtensions = true;
 }
