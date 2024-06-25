db4a478973f70505096a3f494b87e27eb9b70999
bbaserdem <baserdemb@gmail.com>
2024-06-25 19:07:25 -0400

    Nixos ~ locale
    Added /UTF-8 identifiers

--- nixos/features/locale.nix
+++ nixos/features/locale.nix
@@ -5,10 +5,10 @@
   ...
 }: {
   i18n.supportedLocales = [
-    "en_US.UTF-8"
-    "en_GB.UTF-8"
-    "tr_TR.UTF-8"
-    "en_DK.UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "en_GB.UTF-8/UTF-8"
+    "tr_TR.UTF-8/UTF-8"
+    "en_DK.UTF-8/UTF-8"
   ];
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
