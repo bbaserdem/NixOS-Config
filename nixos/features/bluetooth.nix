754b06efe539770aec01f96229d143a27fc914de
bbaserdem <baserdemb@gmail.com>
2024-06-25 16:58:08 -0400

    Nixos - Headset mode
    Trying to disable headset mode in bluetooth

--- nixos/features/bluetooth.nix
+++ nixos/features/bluetooth.nix
@@ -6,4 +6,13 @@
   ...
 }: {
   services.blueman.enable = true;
+  # Disable HSP/HSF mode
+  services.pipewire.wireplumber.extraConfig = {
+    "wireplumber.settings" = {
+      "bluetooth.autoswitch-to-headset-profile" = false;
+    };
+    "monitor.bluez.properties" = {
+      "bluez5.roles" = [ "a2dp_sink" "a2dp_source" ];
+    };
+  };
 }
