9d6890f157ca94c78b96258e6181a92c74427d0a
bbaserdem <baserdemb@gmail.com>
2024-10-23 08:31:25 -0500

    Home - CSHL ssh
    Removed CSHL ssh configuration

--- home-manager/batuhan/configs/ssh/default.nix
+++ home-manager/batuhan/configs/ssh/default.nix
@@ -14,36 +14,6 @@
         identitiesOnly = true;
         extraOptions.IdentityFile = "~/.ssh/id_ed25519_GITHUB";
       };
-      ellipsis = {
-        user = "batu";
-        hostname = "ellipsis.cshl.edu";
-        identitiesOnly = true;
-        extraOptions = {
-          HostKeyAlgorithms = "ssh-rsa";
-          IdentityFile = "~/.ssh/id_rsa_ELLIPSIS";
-          PubkeyAcceptedAlgorithms = "+ssh-rsa";
-          PubkeyAcceptedKeyTypes = "+ssh-rsa";
-        };
-      };
-      hopper = {
-        # Not working for some reason
-        user = "batu";
-        hostname = "hopper.cshl.edu";
-        identitiesOnly = true;
-        extraOptions = {
-          HostKeyAlgorithms = "ssh-rsa";
-          IdentityFile = "~/.ssh/id_rsa_HOPPER";
-          PubkeyAcceptedAlgorithms = "+ssh-rsa";
-          PubkeyAcceptedKeyTypes = "+ssh-rsa";
-        };
-      };
-      haibara = {
-        # Not working for some reason
-        user = "batuhan";
-        hostname = "haibara.cshl.edu";
-        identitiesOnly = true;
-        extraOptions.IdentityFile = "~/.ssh/id_ed25519_HAIBARA";
-      };
     };
   };
 }
