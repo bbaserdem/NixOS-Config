a14e4d56805b55322695a68cc58818320c1d45e9
bbaserdem <baserdemb@gmail.com>
2024-10-23 13:12:11 -0500

    Home manager version->stable
    Upgraded home-manager version

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
