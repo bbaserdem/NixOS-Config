83ea740261ba27db90be37417787d78a3b8633fb
bbaserdem <baserdemb@gmail.com>
2024-10-23 08:41:17 -0500

    Nixos + auto garbage collect
    Added garbage collection automatically

--- nixos/bundles/nix.nix
+++ nixos/bundles/nix.nix
@@ -7,6 +7,13 @@
 }: {
   # This needs disabling for nix-index flake to work
   programs.command-not-found.enable = false;
+  # Automatic garbage collection
+  nix.gc = {
+    automatic = true;
+    frequency = "weekly";
+    options = "-d";
+  };
+  # Nix helper utilities
   environment.systemPackages = with pkgs; [
     nh
     nix-output-monitor
