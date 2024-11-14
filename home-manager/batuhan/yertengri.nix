0569d980ebed6667c0c9a17c2a64fab2487bc653
bbaserdem <baserdemb@gmail.com>
2024-11-13 19:54:11 -0600

    Home + yertengri
    Added yertengri in home-manager

---
+++ home-manager/batuhan/yertengri.nix
@@ -0,0 +1,17 @@
+# batuhan@umay home configuration
+{
+  lib,
+  config,
+  ...
+}: {
+  # Just default to regular now
+  imports = [
+    ./default.nix
+  ];
+  # Create data symlinks
+  home.file."Media".source = config.lib.file.mkOutOfStoreSymlink "/home/data/Media";
+  home.file."Shared/Android".source = config.lib.file.mkOutOfStoreSymlink "/home/data/Android";
+  home.file."Work".source = config.lib.file.mkOutOfStoreSymlink "/home/work";
+  # Disable autorandr in gnome for now
+  services.autorandr.enable = lib.mkOverride 500 false;
+}
