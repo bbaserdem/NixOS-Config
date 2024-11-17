f8639b2bd6723f5bb728f14cd77ace291b8e6368
bbaserdem <baserdemb@gmail.com>
2024-11-16 21:51:01 -0600

    Home ~ typo
    Fixing typo in home folder

--- home-manager/batuhan/yertengri.nix
+++ home-manager/batuhan/yertengri.nix
@@ -11,7 +11,7 @@
   # Create data symlinks
   home.file."Media".source = config.lib.file.mkOutOfStoreSymlink "/home/data/Media";
   home.file."Shared/Android".source = config.lib.file.mkOutOfStoreSymlink "/home/data/Android";
-  home.file."Work".source = config.lib.file.mkOutOfStoreSymlink "/home/work";
+  home.file."Work".source = config.lib.file.mkOutOfStoreSymlink "/home/work/Work";
   # Disable autorandr in gnome for now
   services.autorandr.enable = lib.mkOverride 500 false;
 }
