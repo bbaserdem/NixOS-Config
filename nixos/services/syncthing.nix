168da71e23c565e105995013a4331c5d7cd1b547
bbaserdem <baserdemb@gmail.com>
2024-11-16 20:40:24 -0600

    Nixos + syncthing
    Added syncthing config

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -1,11 +1,111 @@
 # Configuring syncthing
 {
   config,
+  pkgs,
   ...
-}: {
+}:
+let
+  # Template function to exclude self inclusions
+  filterHost = _attr: removeAttrs _attr [ config.networking.hostName ];
+  removeHost = _list: pkgs.lib.lists.remove config.networking.hostName _list;
+  hostsList = [ "yel-ana" "yertengri" ];
+in {
   services.syncthing = {
     enable = true;
     user = config.myNixOS.defaultUser;
+    # Credentials to be set by users
+    key = config.sops.secrets."syncthing/key".path;
+    cert = config.sops.secrets."syncthing/key".cert;
+    # Behavior
+    overrideFolders = true;
+    overrideDevices = true;
+    openDefaultPorts = true;
+    guiAddress = "127.0.0.1:8080";
+    # Settings for the syncthing service among hosts
+    settings = {
+      # Runtime options
+      options = {
+        urAccepted = 1;
+        relaysEnabled = true;
+        localAnnounceEnabled = true;
+      };
+      devices = filterHost {
+        yel-ana = {
+          name = "Yel Ana";
+          id = "DRKKHJC-XAHARAB-JEMMSOW-4IU2OAH-6NMU6SI-DTVDCD2-2RLUPX2-EWQNHQN";
+          autoAcceptFolders = true;
+        };
+        yertengri = {
+          name = "Yertengri";
+          id = "";
+          autoAcceptFolders = true;
+        };
+        erlik = {
+          name = "Erlik";
+          id = "DTELBJI-F7UOJXY-5DCXOKU-EL3DHZC-S7S7K4H-AFLV3KD-HBSNRUY-W6QOEQH";
+          autoAcceptFolders = false;
+        };
+      };
+      folders = {
+        media = {
+          label = "Media";
+          id = "Media_batuhan";
+          path = "~/Media";
+          type = "sendreceive";
+          devices = removeHost [
+            "yel-ana"
+            "yertengri"
+          ];
+          versioning = {
+            type = "trashcan";
+            params = { cleanoutDays = "60"; };
+          };
+        };
+        downloads = {
+          label = "Downloads";
+          id = "Downloads_batuhan";
+          path = "~/Sort/Downloads";
+          type = "sendreceive";
+          devices = removeHost [
+            "yel-ana"
+            "yertengri"
+          ];
+          versioning = {
+            type = "trashcan";
+            params = { cleanoutDays = "10"; };
+          };
+        };
+        work = {
+          label = "Work";
+          id = "Work_batuhan";
+          path = "~/Work";
+          type = "sendreceive";
+          devices = removeHost [
+            "yel-ana"
+            "yertengri"
+          ];
+          versioning = {
+            type = "trashcan";
+            params = { cleanoutDays = "100"; };
+          };
+        };
+        phone = {
+          label = "Android";
+          id = "Android_batuhan";
+          path = "~/Shared/Android";
+          type = "sendreceive";
+          devices = removeHost [
+            "yel-ana"
+            "yertengri"
+            "erlik"
+          ];
+          versioning = {
+            type = "trashcan";
+            params = { cleanoutDays = "30"; };
+          };
+        };
+      };
+    };
     # Relay service
     relay = {
       enable = false;
