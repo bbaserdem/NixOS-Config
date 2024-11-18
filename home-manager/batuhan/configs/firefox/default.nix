874b0ff8e49610205548813a936d37ba7062e50a
bbaserdem <baserdemb@gmail.com>
2024-11-17 21:54:00 -0600

    Home ~ firefox
    Changed some firefox stuff

--- home-manager/batuhan/configs/firefox/default.nix
+++ home-manager/batuhan/configs/firefox/default.nix
@@ -1,7 +1,6 @@
 # Setup browsers to be used
 # TODO: Zotero connector as addon, and vdhcoapp as package
 {
-  config,
   pkgs,
   ...
 }: {
@@ -52,35 +51,65 @@
               {
                 template = "https://search.nixos.org/packages";
                 params = [
-                  {
-                    name = "type";
-                    value = "packages";
-                  }
-                  {
-                    name = "channel";
-                    value = pkgs.system;
-                  }
-                  {
-                    name = "query";
-                    value = "{searchTerms}";
-                  }
+                  { name = "type"; value = "packages"; }
+                  { name = "channel"; value = pkgs.system; }
+                  { name = "sort"; value = "relevance"; }
+                  { name = "query"; value = "{searchTerms}"; }
                 ];
               }
             ];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = ["@np"];
           };
+          "NixOS Options" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/options";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "channel"; value = pkgs.system; }
+                  { name = "sort"; value = "relevance"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@no"];
+          };
           "NixOS Wiki" = {
-            urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+            urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
             iconUpdateUrl = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000;
             definedAliases = ["@nw"];
           };
+          "Home Manager Options" = {
+            urls = [{
+              template = "https://home-manager-options.extranix.com/";
+              params = [{ name = "query"; value = "{searchTerms}"; }];
+            }];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@hm"];
+          };
           "Arch Wiki" = {
             urls = [{template = "https://wiki.archlinux.org/index.php?search={searchTerms}";}];
-            iconUpdateUrl = "https://nixos.wiki/favicon.ico";
+            iconUpdateUrl = "https://wiki.archlinux.org/favicon.ico";
             updateInterval = 24 * 60 * 60 * 1000;
             definedAliases = ["@aw"];
+          };
+          "Gentoo Wiki" = {
+            urls = [{template = "https://wiki.gentoo.org/?search={searchTerms}";}];
+            iconUpdateUrl = "https://wiki.gentoo.org/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000;
+            definedAliases = ["@ge"];
+          };
+          "Wikipedia" = {
+            urls = [{
+              template = "https://en.wikipedia.org/w/index.php";
+                params = [{ name = "search"; value = "{searchTerms}"; }];
+            }];
+            iconUpdateUrl = "https://en.wikipedia.org/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000;
+            definedAliases = ["@wi"];
           };
           "Dotapedia" = {
             urls = [{template = "https://liquipedia.net/dota2/index.php?search={searchTerms}";}];
@@ -88,6 +117,7 @@
             updateInterval = 24 * 60 * 60 * 1000;
             definedAliases = ["@d2"];
           };
+          "Google".metaData.alias = "@g";
         };
       };
       #containers = {
