b58c1c0b5c42e6188ce51e08f161cf6c725e47a9
bbaserdem <baserdemb@gmail.com>
2024-12-21 13:40:07 -0600

    GDM - auto-login
    Turning off auto login in gdm

--- nixos/features/gnome.nix
+++ nixos/features/gnome.nix
@@ -9,29 +9,20 @@
   rootPath,
   ...
 }: {
-  services.displayManager = {
-    # Enable auto login
-    autoLogin = {
-      enable = true;
-      user = "batuhan";
-    };
-  };
   services.xserver = {
     # Enable the X11 windowing system.
     enable = true;
     # Enable gnome login
     displayManager = {
-      gdm.enable = true;
+      gdm = {
+        enable = true;
+        settings = {};
+      };
     };
     # Enable gnome
     desktopManager = {
       gnome.enable = true;
     };
-  };
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services = {
-    "getty@tty1".enable = false;
-    "autovt@tty1".enable = false;
   };
 
   # Exclude some unneeded packages
