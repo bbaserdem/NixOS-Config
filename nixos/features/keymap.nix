ddcbe5dcffa640cecfb460e89bc71499e3b5a4cd
bbaserdem <baserdemb@gmail.com>
2024-12-21 14:11:26 -0600

    Nixos ~ keyboard
    Fixed typo

--- nixos/features/keymap.nix
+++ nixos/features/keymap.nix
@@ -8,7 +8,7 @@
   services.xserver.xkb = {
     layout = "us,tr,us";
     variant = "dvorak-alt-intl,f,altgr-intl";
-    options = ["grp:alt_caps_toggle"];
+    options = "grp:alt_caps_toggle";
   };
   # Console keymap
   console.keyMap = "dvorak";
