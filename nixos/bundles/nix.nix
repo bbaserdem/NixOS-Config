2dfe267ac903f0317976a53c6e6bcd9f08f242d3
bbaserdem <baserdemb@gmail.com>
2024-12-29 15:13:16 -0500

    NixOS ~ nix
    Edited some options

--- nixos/bundles/nix.nix
+++ nixos/bundles/nix.nix
@@ -9,13 +9,18 @@
   # This needs disabling for nix-index flake to work
   programs.command-not-found.enable = false;
   # Automatic garbage collection
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 60d";
+  nix = {
+    nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 60d";
+    };
+    extraOptions = ''
+      keep-outputs = true
+      keep-derivations = true
+    '';
   };
-  # Make sure <nixpkgs> path aligns with flake inputs
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   # Nix helper utilities
   environment.systemPackages = with pkgs; [
     nh
