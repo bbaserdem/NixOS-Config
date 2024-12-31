0a5b629ce79d570ebc8499917f1a12fcefcd89a4
bbaserdem <baserdemb@gmail.com>
2024-12-31 17:38:25 -0500

    Home ~ mail
    Enabled programs for mail services

--- home-manager/batuhan/configs/email/default.nix
+++ home-manager/batuhan/configs/email/default.nix
@@ -16,4 +16,18 @@
     maildirBasePath = "${config.xdg.dataHome}/mail";
   };
 
+  # Specific apps usage
+  programs.lieer.enable = true;
+  programs.msmtp.enable = true;
+  programs.astroid = {
+    enable = true;
+    externalEditor = null;
+    pollScript = "";
+  };
+  services.imapnotify.enable = true;
+
+  # Neomutt config
+  programs.neomutt = {
+    enable = true;
+  };
 }
