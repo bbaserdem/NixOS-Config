08a55b4519810177e0499d9d6996e6177a57d990
bbaserdem <baserdemb@gmail.com>
2025-01-02 18:28:20 -0500

    Home ~ mail
    Fixing mailbox name stuff

--- home-manager/batuhan/configs/email/accounts/gmailGenInbox.nix
+++ home-manager/batuhan/configs/email/accounts/gmailGenInbox.nix
@@ -1,30 +1,30 @@
 account: [
     {
-      name = "├─📥 Inbox";
+      name = "├─📥 Inbox──────[${account}]";
       query = "(tag:inbox -tag:promotions -tag:social) OR (tag:inbox and tag:flagged)";
       limit = 1000;
     } {
-      name = "├─🏦 Archive";
+      name = "├─🏦 Archive────[${account}]";
       query = "not tag:inbox and not tag:spam";
       limit = 1000;
     } {
-      name = "├─🧑 Personal";
+      name = "├─🧑 Personal───[${account}]";
       query = "tag:personal";
       limit = 1000;
     } {
-      name = "├─🚩 Flagged";
+      name = "├─🚩 Flagged────[${account}]";
       query = "tag:flagged";
       limit = 1000;
     } {
-      name = "├─💸 Promotions";
+      name = "├─💸 Promotions─[${account}]";
       query = "tag:promotions";
       limit = 1000;
     } {
-      name = "├─🐦 Social";
+      name = "├─🐦 Social─────[${account}]";
       query = "tag:social";
       limit = 1000;
     } {
-      name = "└─📤 Sent";
+      name = "└─📤 Sent───────[${account}]";
       query = "tag:sent";
       limit = 1000;
     }
