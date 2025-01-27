eb79d9ca8ffa1c6c8ed82c66a2432f2e60ee6454
bbaserdem <baserdemb@gmail.com>
2025-01-26 19:03:16 -0600

    Nixos kde->plasma
    Changed naming

--- nixos/features/plasma.nix
+++ nixos/features/plasma.nix
@@ -1,13 +1,6 @@
 # NixOS: nixos/features/kde.nix
 {pkgs, ...}: {
 
-  # Enable sddm
-  services.xserver.enable = true;
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-
   # Enable plasma
   services.desktopManager.plasma6.enable = true;
 
