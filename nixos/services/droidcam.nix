2343100cbc422b510987351adaf901c1383d7500
bbaserdem <baserdemb@gmail.com>
2025-04-30 08:10:33 -0500

    Nixos ~ droidcam
    Added droidcam settings

--- nixos/services/droidcam.nix
+++ nixos/services/droidcam.nix
@@ -1,5 +1,18 @@
 # nixos/features/droidcam.nix
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Add loopback kernel module
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.kernelModules = [
+    "v4l2loopback"
+    "snd-aloop"
+  ];
+
   # Enable droidcam modules
   programs.droidcam.enable = true;
 
@@ -9,4 +22,9 @@
   # Open ports; might not be needed
   networking.firewall.allowedTCPPorts = [4747];
   networking.firewall.allowedUDPPorts = [4747];
+
+  # Enable v4l2loopback GUI utils
+  environment.systemPackages = with pkgs; [
+    v4l-utils
+  ];
 }
