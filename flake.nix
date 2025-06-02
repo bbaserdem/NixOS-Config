0864d392deb3251aad098c27aa58803e1b99f893
bbaserdem <baserdemb@gmail.com>
2025-06-02 09:45:38 -0500

    Version update
    24.11 -> 25.05

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
   inputs = {
     # ----- System Flakes ----- #
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Nix user repository
@@ -78,7 +78,7 @@
     # ----- Flair and small functionality ----- #
     # Nixifying theming and styling
     stylix = {
-      url = "github:danth/stylix/release-24.11";
+      url = "github:danth/stylix/release-25.05";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         home-manager.follows = "home-manager";
