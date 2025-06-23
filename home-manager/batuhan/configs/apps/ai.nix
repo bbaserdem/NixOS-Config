194ce1f45ad7e42db67871ada0ee5fbad20ee40d
bbaserdem <baserdemb@gmail.com>
2025-06-23 14:42:29 -0500

    Home ~ claude
    Added claude to home manager

--- home-manager/batuhan/configs/apps/ai.nix
+++ home-manager/batuhan/configs/apps/ai.nix
@@ -5,10 +5,9 @@
   config,
   pkgs,
   ...
-} {
-
+}: {
   # Get cursor editor, custom overlay already applied
-  # Also get anthropic's claude 
+  # Also get anthropic's claude
   home.packages = with pkgs; [
     code-cursor
     unstable.claude-code
