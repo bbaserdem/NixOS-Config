d8e4b506d7ce4613986e9a93f50348d7f9fcbfdf
bbaserdem <baserdemb@gmail.com>
2025-07-07 17:16:59 -0500

    NixOS ~ docker
    Added rootless docker module

--- nixos/services/docker.nix
+++ nixos/services/docker.nix
@@ -3,9 +3,8 @@
 {
   config,
   pkgs,
-  ...,
+  ...
 }: {
-
   # Add rootless docker
   virtualisation.docker = {
     # Disable system wide docker
@@ -17,5 +16,5 @@
     };
   };
 
-  users.users.${config.myNixOS.userName}.extraGroups = [ "docker" ];
+  users.users.${config.myNixOS.userName}.extraGroups = ["docker"];
 }
