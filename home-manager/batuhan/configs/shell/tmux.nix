197b161ed13062a5287c68160dd81e364cc04ce7
bbaserdem <baserdemb@gmail.com>
2025-07-21 22:00:55 -0500

    Home ~ direnv
    Trying to reorg direnv

--- home-manager/batuhan/configs/shell/tmux.nix
+++ home-manager/batuhan/configs/shell/tmux.nix
@@ -4,15 +4,19 @@
   stylix.targets.tmux.enable = true;
 
   # Settings
-  programs.tmux = {
-    enable = true;
-    baseIndex = 0;
-    clock24 = true;
-    mouse = true;
-    prefix = "C-g";
-    sensibleOnTop = true;
-    tmuxinator.enable = true;
-    tmuxp.enable = true;
-    enableShellIntegration = true;
+  programs = {
+    tmux = {
+      enable = true;
+      baseIndex = 0;
+      clock24 = true;
+      mouse = true;
+      prefix = "C-g";
+      sensibleOnTop = true;
+      tmuxinator.enable = true;
+      tmuxp.enable = true;
+    };
+    fzf.tmux = {
+      enableShellIntegration = true;
+    };
   };
 }
