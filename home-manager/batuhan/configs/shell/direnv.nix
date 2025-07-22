230d81b4ceafd8e0d773a0167c3f7db442fd974a
bbaserdem <baserdemb@gmail.com>
2025-07-22 18:21:45 -0500

    Home ~ direnv
    Trying to fix direnv stuff

--- home-manager/batuhan/configs/shell/direnv.nix
+++ home-manager/batuhan/configs/shell/direnv.nix
@@ -4,17 +4,32 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  logFormat = "$'\\e[2;1;3mdirenv:\\e[22;23m %s\\e[0m'";
+in {
   # Enable direnv for our shells
   programs.direnv = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
+    config = {
+      global = {
+        load_dotenv = false;
+        warn_timeout = "0";
+        log_format = logFormat;
+      };
+      whitelist = {
+        prefix = [
+          config.xdg.userDirs.extraConfig.XDG_PROJECT_DIR
+        ];
+        exact = [];
+      };
+    };
   };
 
   # Reformat direnv output to be muted
   home.sessionVariables = {
-    "DIRENV_LOG_FORMAT" = "$'\\e[2;1;3mdirenv:\\e[22;23m %s\\e[0m'";
+    "DIRENV_LOG_FORMAT" = logFormat;
   };
 }
