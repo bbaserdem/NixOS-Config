a01dac02448d837a86ebe4d8f2707d6401a4e461
bbaserdem <baserdemb@gmail.com>
2025-08-08 16:45:52 -0500

    Home ~ wayland
    Added qt5 wayland libraries

--- home-manager/batuhan/configs/apps/default.nix
+++ home-manager/batuhan/configs/apps/default.nix
@@ -17,62 +17,67 @@
     ./zathura.nix
   ];
 
-  home.packages = with pkgs; [
-    # Image editing
-    darktable
-    inkscape
-    gimp-with-plugins
-    vimiv-qt
-    qimgv
-    imagemagick
-    blender
-    digikam
-    # Music related
-    audacity
-    musescore
-    picard
-    projectm-sdl-cpp
-    chromaprint
-    # Video related
-    vlc
-    smplayer
-    handbrake
-    flowblade
-    # Communication
-    signal-desktop
-    ferdium
-    slack
-    zoom-us
-    remmina
-    thunderbird
-    # Documents
-    calibre
-    libreoffice-fresh
-    hunspell
-    hunspellDicts.en_US
-    kdePackages.okular
-    obsidian
-    zotero
-    #texlive.combined.scheme-full
-    # Programming gui's
-    octaveFull
-    spyder
-    gitg
-    # Utilities
-    nemo-with-extensions
-    virt-manager
-    baobab
-    xclicker
-    wifi-qr
-    dconf2nix
-    exiftool
-    fzf
-    wl-clipboard
-    tree
-    # hunspellDicts.tr_TR not available
-    # Apps to setup
-    podgrab
-  ];
+  home.packages =
+    (with pkgs; [
+      # Image editing
+      darktable
+      inkscape
+      gimp-with-plugins
+      vimiv-qt
+      qimgv
+      imagemagick
+      blender
+      digikam
+      # Music related
+      audacity
+      musescore
+      picard
+      projectm-sdl-cpp
+      chromaprint
+      # Video related
+      vlc
+      smplayer
+      handbrake
+      flowblade
+      # Communication
+      signal-desktop
+      ferdium
+      slack
+      zoom-us
+      remmina
+      thunderbird
+      # Documents
+      calibre
+      libreoffice-fresh
+      hunspell
+      hunspellDicts.en_US
+      kdePackages.okular
+      obsidian
+      zotero
+      #texlive.combined.scheme-full
+      # Programming gui's
+      octaveFull
+      spyder
+      gitg
+      # Utilities
+      nemo-with-extensions
+      virt-manager
+      baobab
+      xclicker
+      wifi-qr
+      dconf2nix
+      exiftool
+      fzf
+      wl-clipboard
+      tree
+      # hunspellDicts.tr_TR not available
+      # Apps to setup
+      podgrab
+    ])
+    ++ (with pkgs.qt5; [
+      # QT5 libraries
+      qtwayland
+    ]);
 
   # Enable stylix for apps
   stylix.targets = {
