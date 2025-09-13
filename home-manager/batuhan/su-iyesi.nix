01967ad8529924863a4bd770887ab609928f51e4
bbaserdem <baserdemb@gmail.com>
2025-09-13 11:47:01 -0500

    Home ~ su-iyesi
    Trying to fix some stuff

--- home-manager/batuhan/su-iyesi.nix
+++ home-manager/batuhan/su-iyesi.nix
@@ -9,20 +9,7 @@
   # Be selective what we import since we are on darwin
   imports = [
     # Module imports
-    #inputs.nixCats.homeManagerModules.default
-    inputs.sops-nix.homeManagerModules.sops
-    inputs.stylix.darwinModules.stylix
-    # My modules to load
-    ./apps/firefox.nix
-    ./apps/kitty.nix
-    #./apps/neovim.nix
-    ./security/sops.nix
-    ./security/ssh.nix
-    ./shell/git.nix
-    ./shell/man.nix
-    ./shell/tmux.nix
-    ./shell/zsh.nix
-    ./theming/stylix.nix
+    inputs.nixCats.homeManagerModules.default
   ];
 
   # Nixpkgs version
