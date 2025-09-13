c15e43f19ebb536725172ee91a16917594b164ee
bbaserdem <baserdemb@gmail.com>
2025-09-13 11:46:26 -0500

    Home ~ su-iyesi
    Trying to fix some stuff

--- home-manager/batuhan/su-iyesi.nix
+++ home-manager/batuhan/su-iyesi.nix
@@ -9,13 +9,13 @@
   # Be selective what we import since we are on darwin
   imports = [
     # Module imports
-    inputs.nixCats.homeManagerModules.default
+    #inputs.nixCats.homeManagerModules.default
     inputs.sops-nix.homeManagerModules.sops
     inputs.stylix.darwinModules.stylix
     # My modules to load
     ./apps/firefox.nix
     ./apps/kitty.nix
-    ./apps/neovim.nix
+    #./apps/neovim.nix
     ./security/sops.nix
     ./security/ssh.nix
     ./shell/git.nix
@@ -24,6 +24,21 @@
     ./shell/zsh.nix
     ./theming/stylix.nix
   ];
+
+  # Nixpkgs version
+  nixpkgs = {
+    overlays = [
+      # My overlays
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+      # External overlays
+      inputs.nur.overlays.default
+    ];
+    config = {
+      allowUnfree = true;
+    };
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "25.05";
