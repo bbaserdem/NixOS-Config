1a419cf1f90256e1b9629e7d1c7008b442980184
bbaserdem <baserdemb@gmail.com>
2025-09-13 11:53:01 -0500

    Home ~ su-iyesi
    Trying to fix some stuff

--- home-manager/batuhan/su-iyesi.nix
+++ home-manager/batuhan/su-iyesi.nix
@@ -13,7 +13,7 @@
     inputs.sops-nix.homeManagerModules.sops
     inputs.stylix.homeManagerModules.stylix
     # My modules to load
-    ./apps/firefox.nix
+    #./apps/firefox.nix
     ./apps/kitty.nix
     #./apps/neovim.nix
     ./security/sops.nix
