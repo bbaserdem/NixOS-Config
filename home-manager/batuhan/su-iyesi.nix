99c5cebcc75fc7ba477e913db87408a0a311e159
bbaserdem <baserdemb@gmail.com>
2025-09-13 11:14:12 -0500

    Home ~ su-iyesi
    Changed some stuff

--- home-manager/batuhan/su-iyesi.nix
+++ home-manager/batuhan/su-iyesi.nix
@@ -13,16 +13,16 @@
     inputs.sops-nix.homeManagerModules.sops
     inputs.stylix.darwinModules.stylix
     # My modules to load
-    ../apps/firefox.nix
-    ../apps/kitty.nix
-    ../apps/neovim.nix
-    ../security/sops.nix
-    ../security/ssh.nix
-    ../shell/git.nix
-    ../shell/man.nix
-    ../shell/tmux.nix
-    ../shell/zsh.nix
-    ../theming/stylix.nix
+    ./apps/firefox.nix
+    ./apps/kitty.nix
+    ./apps/neovim.nix
+    ./security/sops.nix
+    ./security/ssh.nix
+    ./shell/git.nix
+    ./shell/man.nix
+    ./shell/tmux.nix
+    ./shell/zsh.nix
+    ./theming/stylix.nix
   ];
 
   # Nixpkgs version
