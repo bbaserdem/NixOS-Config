c80d98ac8eab9b5bfe09f5c7d48a5c0ce061be97
bbaserdem <baserdemb@gmail.com>
2025-09-13 15:30:09 -0500

    Home ~ su-iyesi
    Trying to fix some stuff

--- home-manager/batuhan/theming/stylix.nix
+++ home-manager/batuhan/theming/stylix.nix
@@ -5,67 +5,66 @@
   config,
   lib,
   ...
-}: {
-  config = lib.mkMerge [
-    {
-      stylix = {
+}:
+lib.mkMerge [
+  {
+    stylix = {
+      enable = true;
+      autoEnable = false;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
+      image = config.myHome.wallpaper.path;
+      polarity = "dark";
+
+      # Opacity options
+      opacity = {
+        applications = 1.0;
+        desktop = 0.9;
+        popups = 0.9;
+        terminal = 0.9;
+      };
+
+      fonts = {
+        monospace = {
+          package = pkgs._3270font;
+          name = "IBM 3270";
+        };
+        serif = {
+          package = pkgs.caladea;
+          name = "Caladea";
+        };
+        sansSerif = {
+          package = pkgs.source-sans-pro;
+          name = "Source Sans Pro";
+        };
+        emoji = {
+          package = pkgs.noto-fonts-color-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
+    };
+  }
+  (lib.mkIf pkgs.stdenv.isLinux {
+    # Icons
+    stylix = {
+      iconTheme = {
         enable = true;
-        autoEnable = false;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
-        image = config.myHome.wallpaper.path;
-        polarity = "dark";
-
-        # Opacity options
-        opacity = {
-          applications = 1.0;
-          desktop = 0.9;
-          popups = 0.9;
-          terminal = 0.9;
-        };
-
-        fonts = {
-          monospace = {
-            package = pkgs._3270font;
-            name = "IBM 3270";
-          };
-          serif = {
-            package = pkgs.caladea;
-            name = "Caladea";
-          };
-          sansSerif = {
-            package = pkgs.source-sans-pro;
-            name = "Source Sans Pro";
-          };
-          emoji = {
-            package = pkgs.noto-fonts-color-emoji;
-            name = "Noto Color Emoji";
-          };
-        };
+        package = pkgs.qogir-icon-theme;
+        dark = "Qogir-dark";
+        light = "Qogir";
       };
-    }
-    (lib.mkIf pkgs.stdenv.isLinux {
-      # Icons
-      stylix = {
-        iconTheme = {
-          enable = true;
-          package = pkgs.qogir-icon-theme;
-          dark = "Qogir-dark";
-          light = "Qogir";
-        };
 
-        # Cursor
-        cursor = {
-          package = pkgs.bibata-cursors;
-          name = "Bibata-Modern-Ice";
-        };
-
-        # Enable themeing systemwide
-        targets = {
-          gtk.enable = true;
-          kde.enable = true;
-          xresources.enable = true;
-        };
+      # Cursor
+      cursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
       };
-    })
-  ];
-}
+
+      # Enable themeing systemwide
+      targets = {
+        gtk.enable = true;
+        kde.enable = true;
+        xresources.enable = true;
+      };
+    };
+  })
+]
