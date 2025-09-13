bd26462b11a543f52f298eaf75ff0e5c0ed9b339
bbaserdem <baserdemb@gmail.com>
2025-09-13 11:49:47 -0500

    Home ~ su-iyesi
    Trying to fix some stuff

--- home-manager/batuhan/su-iyesi.nix
+++ home-manager/batuhan/su-iyesi.nix
@@ -11,6 +11,18 @@
     # Module imports
     inputs.nixCats.homeManagerModules.default
     inputs.sops-nix.homeManagerModules.sops
+    inputs.stylix.homeManagerModules.stylix
+    # My modules to load
+    ./apps/firefox.nix
+    ./apps/kitty.nix
+    #./apps/neovim.nix
+    ./security/sops.nix
+    ./security/ssh.nix
+    ./shell/git.nix
+    ./shell/man.nix
+    ./shell/tmux.nix
+    ./shell/zsh.nix
+    ./theming/stylix.nix
   ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
