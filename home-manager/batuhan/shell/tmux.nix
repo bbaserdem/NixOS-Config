ecb66e2c12410de215bb2c4fa5a26bffea530701
bbaserdem <baserdemb@gmail.com>
2025-10-13 18:43:04 -0500

    Home ~ tmux
    Added better tmux shortcuts

--- home-manager/batuhan/shell/tmux.nix
+++ home-manager/batuhan/shell/tmux.nix
@@ -10,10 +10,30 @@
       baseIndex = 0;
       clock24 = true;
       mouse = true;
-      prefix = "C-g";
+      prefix = "C-a";
       sensibleOnTop = true;
       tmuxinator.enable = true;
       tmuxp.enable = true;
+      extraConfig = ''
+        # Split panes
+        bind | split-window -h
+        bind - split-window -v
+        unbind '"'
+        unbind %
+
+        # Change panes with Alt-arrow
+        bind -n M-Left  select-pane -L
+        bind -n M-Right select-pane -R
+        bind -n M-Up    select-pane -U
+        bind -n M-Down  select-pane -D
+
+        # Turn off bell
+        set -g visual-activity off
+        set -g visual-bell off
+        set -g visual-silence off
+        set -g bell-action none
+        setw -g monitor-activity off
+      '';
     };
     fzf.tmux = {
       enableShellIntegration = true;
