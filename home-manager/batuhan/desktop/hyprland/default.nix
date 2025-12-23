79ece75fa5b44db9a10faf3c1786c65a735d187f
bbaserdem <baserdemb@gmail.com>
2025-12-22 19:10:31 -0600

    Home ~ hyprland
    Testing hyprland stuff

--- home-manager/batuhan/desktop/hyprland/default.nix
+++ home-manager/batuhan/desktop/hyprland/default.nix
@@ -1,22 +1,41 @@
 # ./home-manager/batuhan/desktop/hyprland/default.nix
 # Hyprland config entry point
-{
-  pkgs,
-  lib,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
-    inputs.dms.homeModules.dankMaterialShell.default
     ./shell.nix
+    ./settings.nix
+    ./keybinds.nix
+    ./idle.nix
+    # ./kanshi.nix
   ];
 
-  # Entry point
+  # Styling
+  stylix.targets.hyprland = {
+    enable = true;
+    colors.enable = true;
+    hyprpaper.enable = true;
+    image.enable = true;
+  };
+
+  # Generic settings
   wayland.windowManager.hyprland = {
     enable = true;
+    # Use OS level packages
+    package = null;
+    portalPackage = null;
     # Needed for uwsm
     systemd.enable = false;
     # We want xwayland
     xwayland.enable = true;
   };
+  services.hyprpaper = {
+    enable = true;
+  };
+
+  # Apps
+  home.packages = with pkgs; [
+    brightnessctl
+    hyprshot
+    hyprpicker
+  ];
 }
---
+++ home-manager/batuhan/desktop/hyprland/monitors.nix
@@ -0,0 +1,30 @@
+# ./home-manager/batuhan/desktop/hyprland/monitors.nix
+# Dynamic monitor layout using kanshi
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  services.kanshi = {
+    enable = true;
+
+    # All possible monitors
+    settings = [
+      {
+        output = {
+          criteria = "";
+        };
+      }
+    ];
+
+    profiles = {
+      "Yel-Ana" = {
+        name = "Yel-Ana (Default)";
+        outputs = [
+          {}
+        ];
+      };
+    };
+  };
+}
