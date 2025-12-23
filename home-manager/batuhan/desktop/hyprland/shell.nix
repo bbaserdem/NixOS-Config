dbe7ff520e967371b3c0cfc7683c74037d7c244e
bbaserdem <baserdemb@gmail.com>
2025-12-23 10:58:45 -0600

    Home ~ hyprland
    Testing hyprland stuff

--- home-manager/batuhan/desktop/hyprland/shell.nix
+++ home-manager/batuhan/desktop/hyprland/shell.nix
@@ -11,5 +11,12 @@
 
   programs.dankMaterialShell = {
     enable = true;
+    # Core features
+    enableSystemMonitoring = false; # System monitoring widgets (dgop)
+    enableClipboard = false; # Clipboard history manager
+    enableVPN = false; # VPN management widget
+    enableDynamicTheming = false; # Wallpaper-based theming (matugen)
+    enableAudioWavelength = false; # Audio visualizer (cava)
+    enableCalendarEvents = false; # Calendar integration (khal)
   };
 }
