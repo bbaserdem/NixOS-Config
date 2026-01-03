fe04a7514a7a529e22bb9424e6a343601e6ce8ad
bbaserdem <baserdemb@gmail.com>
2026-01-02 18:30:29 -0600

    Beets - fixes
    Fixing beets

--- home-manager/batuhan/media/beets.nix
+++ home-manager/batuhan/media/beets.nix
@@ -11,19 +11,54 @@
   # Beets config
   programs.beets = {
     enable = lib.mkDefault true;
-    package = pkgs.python3.pkgs.beets.override {
-      pluginOverrides = {
-        alternatives = {
-          enable = true;
-          propagatedBuildInputs = [pkgs.python3Packages.beets-alternatives];
+    package = let
+      py = pkgs.unstable.python3.pkgs;
+      # Override beets-copyartifacts to version 0.1.6
+      beets-copyartifacts-updated = py.beets-copyartifacts.overridePythonAttrs (old: rec {
+        version = "0.1.6";
+        src = pkgs.fetchFromGitHub {
+          owner = "adammillerio";
+          repo = "beets-copyartifacts";
+          rev = "v${version}";
+          hash = "sha256-fMnXuMwxylO9Q7EFPpkgwwNeBuviUa8HduRrqrqdMaI=";
         };
-        # TODO: Marked as broken, fix this
-        copyartifacts = {
-          enable = true;
-          propagatedBuildInputs = [pkgs.python3Packages.beets-copyartifacts];
+        meta = old.meta // { broken = false; };
+        # Disable tests as they're incompatible with newer beets versions
+        doCheck = false;
+      });
+      # Override beets-alternatives to version 0.14.0
+      beets-alternatives-updated = py.beets-alternatives.overridePythonAttrs (old: rec {
+        version = "0.14.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "geigerzaehler";
+          repo = "beets-alternatives";
+          rev = "v${version}";
+          hash = "sha256-leZYXf6Oo/jAKbnJbP+rTnuRsh9P1BQXYAbthMNT60A=";
         };
-      };
-    };
+        # Remove patches since the fix should be mainlined in v0.14.0
+        patches = [];
+        # v0.14.0 uses hatchling as build system
+        build-system = [py.hatchling];
+      });
+    in
+      (py.beets.override {
+        pluginOverrides = {
+          alternatives = {
+            enable = true;
+            propagatedBuildInputs = [beets-alternatives-updated];
+          };
+          copyartifacts = {
+            enable = true;
+            propagatedBuildInputs = [beets-copyartifacts-updated];
+          };
+        };
+      }).overrideAttrs (oldAttrs: {
+        # Work around nixpkgs bug where plugin dependencies aren't properly included
+        propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
+          beets-alternatives-updated
+          beets-copyartifacts-updated
+        ];
+      });
     mpdIntegration = {
       enableStats = true;
       enableUpdate = true;
