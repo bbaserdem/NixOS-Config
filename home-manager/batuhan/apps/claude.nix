d52adb8371a16bbf3e9d7062c30e944563103bd1
bbaserdem <baserdemb@gmail.com>
2026-02-11 09:46:11 -0600

    Home ~ Claude
    Setting up claude

--- home-manager/batuhan/apps/claude.nix
+++ home-manager/batuhan/apps/claude.nix
@@ -1,19 +1,24 @@
 # List of userspace applications
 {...}: {
+  # Import claude settings for hooks
   imports = [
     ./claude
   ];
+
   # Claude code config
   programs.claude-code = {
+    # Enable configuration without installing, we will install manually
     enable = true;
+    package = null;
+
+    # Configuration
     agentsDir = ./claude/agents;
     hooksDir = ./claude/hooks;
     skillsDir = ./claude/skills;
     commandsDir = ./claude/commands;
-    #memory.source = ./CLAUDE.md;
-    mcpServers = {
-    };
+    mcpServers = {};
 
+    # Settings to go to settings.json
     settings = {
       includeCoAuthoredBy = false;
     };
