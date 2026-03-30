ba62bc1091a5ea8e535f7b1145b90338d575878d
bbaserdem <baserdemb@gmail.com>
2026-03-30 09:31:05 -0500

    Home + fcitx

--- home-manager/batuhan/desktop/keyboard.nix
+++ home-manager/batuhan/desktop/keyboard.nix
@@ -34,25 +34,112 @@
       waylandFrontend = true;
       addons = with pkgs; [
         kdePackages.fcitx5-qt
-        libsForQt5.fcitx5-qt
         fcitx5-gtk
       ];
       quickPhrase = {
         # Emojis
         ":evilsmirk:" = "😈";
       };
-      settings.addons = {
-        quickphrase = {
-          globalSection = {
-            "Choose Modifier" = "None";
-            Spell = true;
-            FallbackSpellLanguage = "en";
+      settings = {
+        # Fcitx5 settings
+
+        # Languages
+        inputMethod = {
+          GroupOrder = {
+            "0" = "English";
+            "1" = "Turkish";
           };
-          sections = {
-            TriggerKey = {
-              "0" = "Control+period";
-              "1" = "Super+grave";
+          "Groups/0" = {
+            Name = "English";
+            "Default Layout" = "us-dvorak-alt-intl";
+            DefaultIM = "keyboard-us-alt-intl";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us-dvorak-alt-intl";
+          "Groups/0/Items/1".Name = "keyboard-us-alt-intl";
+          "Groups/1" = {
+            Name = "Turkish";
+            "Default Layout" = "tr-f";
+            DefaultIM = "keyboard-tr-intl";
+          };
+          "Groups/1/Items/0".Name = "keyboard-tr-f";
+          "Groups/1/Items/1".Name = "keyboard-tr-intl";
+        };
+
+        # Shortcuts;
+        # Super + ` : Switches layout
+        # Ctrl + Super + ` : Switches language
+        globalOptions = {
+          Hotkey = {
+            ModifierOnlyKeyTimeout = 250;
+            "Hotkey/TriggerKeys"."0" = "Control+Super+space";
+            "Hotkey/EnumerateForwardKeys"."0" = "Super+grave";
+            "Hotkey/EnumerateBackwardKeys"."0" = "Super+asciitilde";
+            "Hotkey/EnumerateGroupForwardKeys"."0" = "Control+Super+grave";
+            "Hotkey/EnumerateGroupBackwardKeys"."0" = "Control+Super+asciitilde";
+            # Switching logic
+            EnumerateWithTriggerKeys = false;
+            EnumerateSkipFirst = false;
+            Behavior = {
+              # State management
+              resetStateWhenFocusIn = "No";
+              ShareInputState = "No";
+              PreeditEnabledByDefault = false;
+              # Visibility
+              ShowInputMethodInformation = true;
+              CompactInputMethodInformation = true;
+              ShowFirstInputMethodInformation = true;
+              showInputMethodInformationWhenFocusIn = false;
+              # Passwords
+              AllowInputMethodForPassword = false;
+              ShowPreeditForPassword = false;
+              # Behavior
+              ActiveByDefault = false;
+              OverrideXkbOption = true;
+              PreloadInputMethod = true;
+              AutoSavePeriod = 30;
             };
+          };
+        };
+
+        # Addons
+        addons = {
+          # Addons
+
+          # Spelling config; default to using enchant
+          # Enchant can use nuspell backend, which is superior in turkish
+          spell = {
+            sections = {
+              ProviderOrder = {
+                "0" = "Enchant";
+                "1" = "Presage";
+                "2" = "Custom";
+              };
+            };
+          };
+
+          # Quickphrase; where emoji is available
+          # Ctrl + . opens quickphrase menu, type and space insert emoji
+          quickphrase = {
+            globalSection = {
+              "Choose Modifier" = "None";
+              FallbackSpellLanguage = "en";
+              Spell = true;
+              "Commit Key" = "Return";
+              "Choose Key" = "Digit";
+            };
+            sections = {
+              TriggerKey = {
+                "0" = "Control+period";
+              };
+            };
+          };
+
+          # Unicode, trigger unicode code entry, or search
+          unicode = {
+            TriggerKey = {
+              "0" = "Control+semicolon";
+            };
+            DirectUnicodeMode."0" = "Control+Shift+U";
           };
         };
       };
