777f2ca7151bfb7b2edd00c3a9479edee6b06df2
bbaserdem <baserdemb@gmail.com>
2026-03-30 14:22:27 -0500

    Home ~ neovim
    Updating neovim to just use new version

--- home-manager/batuhan/apps/neovim.nix
+++ home-manager/batuhan/apps/neovim.nix
@@ -1,86 +1,22 @@
 # Neovim config
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
-} @ args: let
-  # Grab stylix override
-  stylix16 =
-    pkgs.lib.filterAttrs
-    (k: v: builtins.match "base0[0-9A-F]" k != null)
-    config.lib.stylix.colors.withHashtag;
-  nvimPkg = config.nixCats.out.packages.nixCats;
-  nvimExe = "${nvimPkg}/bin/${nvimPkg.nixCats_packageName}";
-in {
+{inputs, ...}: {
+  # Import the editor module
   imports = [
-    inputs.dendriticFlake.modules.homeManager.neovim
+    inputs.dendriticFlake.modules.homeManager.editor
   ];
-  config = lib.mkMerge [
-    # Main module
-    {
-      # Neovim nixCats
-      nixCats = {
-        enable = true;
-        nixpkgs_version = inputs.nixpkgs-unstable;
-        packageNames = ["nixCats"];
-        packageDefinitions.replace = {
-          nixCats = {pkgs, ...}: {
-            extra = {
-              # Pass configuration to nixd
-              nix = {
-                inherit (args) host user;
-                flake = outputs.lib.rootDir;
-              };
-              colorscheme = {
-                # Inject the base16 colorset to the stylix theme (uses mini.base16)
-                base16 = stylix16;
-                translucent = false;
-                # Set dark and light colorschemes
-                dark = "kanagawa-dragon";
-                light = "catppuccin-latte";
-              };
-            };
-          };
-        };
-      };
 
-      # Make us the default
-      home.sessionVariables.EDITOR = nvimExe;
-    }
-    # New neovim
-    {
-      wrappers.neovim = {...}: {
-        enable = true;
-        # Set default theme
-        settings = {
-          colorscheme = {
-            dark = "onedark";
-            light = "kanagawa-lotus";
-            translucent = false;
-            default = "dark";
-          };
+  # Customize package
+  config = {
+    wrappers.neovim = {
+      # Set default theme
+      settings = {
+        colorscheme = {
+          dark = "onedark";
+          light = "kanagawa-lotus";
+          translucent = false;
+          default = "dark";
         };
       };
-    }
-    (lib.mkIf (pkgs.stdenv.hostPlatform.isLinux) {
-      # Enable neovide; ide for neovim
-      stylix.targets.neovide.enable = true;
-      programs.neovide = {
-        enable = true;
-        settings = {
-          neovim-bin = nvimExe;
-          fork = true;
-          frame = "full";
-          idle = true;
-          mouse-cursor-icon = "arrow";
-          no-multigrid = false;
-          tabs = false;
-          theme = "auto";
-        };
-      };
-    })
-  ];
+    };
+  };
 }
--- home-manager/batuhan/default.nix
+++ home-manager/batuhan/default.nix
@@ -53,5 +53,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "25.05";
+  home.stateVersion = "25.11";
 }
